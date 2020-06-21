79768e3a29669adf73714a4a490fdc4148c0b9f1
aadityayadav <67205038+aadityayadav@users.noreply.github.com>
2020-06-21 23:55:16 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>login page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div id="frm">
+      <form action="process.php" method="post">
+        <p>
+          <label>Username:</label>
+
+        </p>
+        <p>
+           <label>Password</label>
+           <input type="password" name="pass" id="pass">
+        </p>
+        <p>
+           <input type="submit" id="btn" value="login">
+        </p>
+      </form>
+    </div>
+  </body>
+</html>
